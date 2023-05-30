c021d93f4da5b3b23615b8b04ee1038f81d7677d
RosileneTi <rosa-clemente@hotmail.com>
2023-05-30 12:14:11 -0300

    criado novo projeto cenário teste

---
+++ Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Cpf { get; set; }
+        public string E_mail { get; set; }
+        public string Senha { get; set; }
+    }
+}
